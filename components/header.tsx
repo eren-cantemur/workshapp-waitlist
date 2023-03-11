99f94586b64914280548573001c5fddd344a1b77
Mücahit Eren Cantemür <eren.cantemur@jekirdek.com>
2023-03-11 18:42:44 +0300

    open new page

--- components/header.tsx
+++ components/header.tsx
@@ -21,12 +21,14 @@ export default function Header() {
 
           <div className="flex items-center lg:order-2">
             <a
+              target="_blank"
               href="https://forms.gle/2SRUTaNFL93QE7fq6"
               className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-md px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Bize katıl
             </a>
             <a
+              target="_blank"
               href="https://api.whatsapp.com/send/?phone=%2B905363104981&text=Merhaba%2C+bilgi+almak+istiyorum.%0A%0A&app_absent=0"
               className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-md px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
             >
