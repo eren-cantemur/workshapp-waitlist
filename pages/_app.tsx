1564f7799a2b5058fa396cbc7a01e94793bc3a76
Mücahit Eren Cantemür <eren.cantemur@jekirdek.com>
2023-03-11 18:38:12 +0300

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider>
       <Notifications />
       <Component {...pageProps} />
+      <Analytics />
     </MantineProvider>
   );
 }
